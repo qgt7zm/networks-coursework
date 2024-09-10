e28ce83b1392a13b0bd5755cbaf4354f2a35da96
qgt7zm <qgt7zm@virginia.edu>
2024-09-10 14:45:07 -0400

    Add debug option and use Packet ack_num

--- hw2/ends.py
+++ hw2/ends.py
@@ -3,8 +3,17 @@ from util import Packet, Message, cancel_timer, create_timer, now, trace
 
 # Constants
 
+DEBUG = True  # Set this to false for autograder
 ACK_PACKET_DATA = b'ACK'
 INITIAL_SEQ_NUM = 0
+NO_ACK_MODE = 'no-ack'
+STOP_WAIT_MODE = 'one-zero'
+
+# Helper Functions
+
+def print_debug(msg: str | None) -> None:
+    if DEBUG and msg:
+        print(msg)
 
 # Class Implementations
 
@@ -19,9 +28,9 @@ class MySender:
     def from_application(self, message: Message) -> bool:
         packet = Packet(data=message.data, is_end=message.is_end, seq_num=self.seq_num)
 
-        if config.MODE == 'no-ack':
+        if config.MODE == NO_ACK_MODE:
             self.to_network(packet)
-        elif config.MODE == 'one-zero':
+        elif config.MODE == STOP_WAIT_MODE:
             if self.waiting:  # don't send if waiting
                 return False
 
@@ -34,10 +43,10 @@ class MySender:
             return
 
         if config.MODE == 'one-zero':
-            if packet.seq_num != self.seq_num:  # make sure ACK has correct seq num
+            if packet.ack_num != self.seq_num:  # make sure ACK has correct seq num
                 return
 
-            print(f"sender got ACK {packet.seq_num}")
+            print_debug(f"sender got ACK {packet.ack_num}")
             self.waiting = False
             self.seq_num = 1 - self.seq_num  # flip the sequence bit
             self.ready_for_more_from_application()  # get the next message
@@ -51,8 +60,7 @@ class MySender:
 
     def send_packet(self, packet: Packet, debug: str=None):
         self.to_network(packet)
-        if debug:
-            print(debug)
+        print_debug(debug)
 
         # Start/reset wait timer
         if self.resend_timer:
@@ -73,12 +81,12 @@ class MyReceiver:
     def from_network(self, packet: Packet) -> None:
         message = Message(data=packet.data, is_end=packet.is_end)
 
-        if config.MODE == 'no-ack':
+        if config.MODE == NO_ACK_MODE:
             self.to_application(message)
-        elif config.MODE == 'one-zero':
+        elif config.MODE == STOP_WAIT_MODE:
             if packet.seq_num != self.last_seq_num:  # forward new messages
                 self.to_application(message)
-                print(f"receiver got {packet.seq_num}")
+                print_debug(f"receiver got {packet.seq_num}")
 
                 self.last_seq_num = packet.seq_num
                 self.seq_num = 1 - self.seq_num  # flip the sequence bit
@@ -86,15 +94,14 @@ class MyReceiver:
                 # Send ACK
                 self.send_ack(packet.is_end, packet.seq_num, f"receiver sent ACK {packet.seq_num}")
             else:  # don't forward duplicate messages
-                print(f"receiver got duplicate {packet.seq_num}")
+                print_debug(f"receiver got duplicate {packet.seq_num}")
 
                 # Resend ACK
                 self.send_ack(packet.is_end, self.last_seq_num, f"receiver resent ACK {packet.seq_num}")
 
     # Helper Functions
 
-    def send_ack(self, is_end:bool, seq_num: int, debug: str=None):
-        ack_packet = Packet(data=ACK_PACKET_DATA, is_end=is_end, seq_num=seq_num)
+    def send_ack(self, is_end:bool, ack_num: int, debug: str=None):
+        ack_packet = Packet(data=ACK_PACKET_DATA, is_end=is_end, ack_num=ack_num)
         self.to_network(ack_packet)
-        if debug:
-            print(debug)
+        print_debug(debug)
