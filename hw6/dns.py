ff0435623656eb47b7c69424479b3b3e4657eb18
qgt7zm <qgt7zm@virginia.edu>
2024-10-28 11:59:27 -0400

    Create HW6 file

---
+++ hw6/dns.py
@@ -0,0 +1,11 @@
+import argparse
+import json
+import struct
+import socket
+
+if __name__ == '__main__':
+    print("Running dns.py")
+    # TODO parse args
+    # TODO send DNS request
+    # TODO read DNS response
+    # TODO create output file
