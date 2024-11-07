8994a25136710b1493fc1f93feefd033c9f64434
qgt7zm <qgt7zm@virginia.edu>
2024-11-07 15:53:07 -0500

    Accept HTTP connection from client

--- hw7/webserver.py
+++ hw7/webserver.py
@@ -2,12 +2,26 @@ import argparse
 import socket
 import struct
 
-if __name__ == '__main__':
+
+def get_args() -> tuple:
     parser = argparse.ArgumentParser()
     parser.add_argument('ip', type=str)  # host IP address
     parser.add_argument('port', type=int)  # host port
     args = parser.parse_args()
+    return args.ip, args.port
 
-    ip = args.ip
-    port = args.port
-    print(f"Running webserver on https://{ip}:{port}")
+
+if __name__ == '__main__':
+    ip, port = get_args()
+    print(f"Running webserver on http://{ip}:{port}")
+
+    # Using IPv4 address
+    # HTTP uses TCP streams
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    server_socket.bind((ip, port))
+    server_socket.listen()
+    connection, address = server_socket.accept()
+    with connection:
+        client_ip, client_port = address
+        print(f"Received connection from {client_ip}:{client_port}")
+    server_socket.close()
