2ad684726ef96fe97ca446aeefde268d32b3f5df
qgt7zm <qgt7zm@virginia.edu>
2024-11-07 17:01:31 -0500

    Get HTTP request method

--- hw7/webserver.py
+++ hw7/webserver.py
@@ -11,6 +11,22 @@ def get_args() -> tuple:
     return args.ip, args.port
 
 
+def process_request(request: bytes) -> dict:
+    print(request)
+    request_op = request[:request.find(b' ')].decode('utf-8')
+
+    if request_op == 'GET':
+        print("using GET")
+        # return 200 OK, 301 redirect, 404 or not found
+    elif request_op == 'HEAD':
+        print("using HEAD")
+    else:
+        # return 405 method not allowed
+        print("invalid op")
+
+    return {}
+
+
 if __name__ == '__main__':
     ip, port = get_args()
     print(f"Running webserver on http://{ip}:{port}")
@@ -18,10 +34,14 @@ if __name__ == '__main__':
     # Using IPv4 address
     # HTTP uses TCP streams
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((ip, port))
     server_socket.listen()
     connection, address = server_socket.accept()
     with connection:
         client_ip, client_port = address
         print(f"Received connection from {client_ip}:{client_port}")
+
+        client_request = connection.recv(1000)
+        process_request(client_request)
     server_socket.close()
