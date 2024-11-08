8839dabccdee7408d4af38944709a3bca7487085
qgt7zm <qgt7zm@virginia.edu>
2024-11-07 23:25:23 -0500

    Respond with correct header and body

--- hw7/webserver.py
+++ hw7/webserver.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import os
 import socket
 import sys
 
@@ -9,8 +10,9 @@ RESPONSE_CODES = {
     200: 'OK',
     301: 'Moved Permanently',
     404: 'Not Found',
-    405 : 'Method Not Allowed'
+    405: 'Method Not Allowed'
 }
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -20,27 +22,21 @@ def get_args():
     return parser.parse_args()
 
 
-def is_path_valid(request_path) -> bool:
-    path = Path('webroot' + request_path)
-    return path.is_file()
-
-
 def get_response_code(method: str, path: str) -> int:
     if method == 'GET' or method == 'HEAD':
-        print("method: " + method)
+        print("Method: " + method)
 
-        if is_path_valid(path):
-            if 'redirect' in path:
-                print(f"redirect: {path}")
+        file_path = Path('webroot' + path)
+        if file_path.is_file():
+            if not os.access(file_path, os.R_OK):
+                return 403  # not authorized
+            elif 'redirect' in path:
                 return 301  # moved permanently
             else:
-                print(f"OK: {path}")
                 return 200  # OK
         else:
-            print(f"not found: {path}")
             return 404  # not found
     else:
-        print(f"invalid method: {method}")
         return 405  # method not allowed
 
 
@@ -48,24 +44,45 @@ def process_request(request: bytes) -> dict:
     request_str = request.decode('utf-8')
     request_lines = request_str.split('\r\n')
 
-    # Get response code from header
+    # Parse the header
     request_header = request_lines[0].split(' ')
-    response_code = get_response_code(request_header[0], request_header[1])
+    request_method = request_header[0]
+    request_path = request_header[1]
+
+    # Get the response code
+    response_code = get_response_code(request_method, request_path)
 
     response_data = {
         'code': response_code
     }
 
+    # Create the response parameters
     if response_code == 200:
-        # TODO get file type
-        pass
+        if request_method == 'GET':
+            # Get the file type
+            file_ext = request_path[request_path.find('.') + 1:]
+            if file_ext == 'html' or file_ext == 'htm':
+                response_data['type'] = 'text/html'
+            else:
+                response_data['type'] = 'text/plain'
+
+            # Read the file
+            file_path = Path('webroot' + request_path)
+            with open(file_path, 'r') as file:
+                contents = file.read()
+                response_data['body'] = contents
+                response_data['length'] = len(contents)
+        else:
+            response_data['body'] = None
     elif response_code == 301:
-        # TODO find redirect target
+        # Get the redirect target
+        response_data['body'] = f"Redirect: {request_path}"
+        response_data['location'] = "/redirect-target.html"
         pass
     elif response_code == 404:
-        pass
-    elif response_code == 504:
-        pass
+        response_data['body'] = f"File not found: {request_path}"
+    elif response_code == 405:
+        response_data['body'] = f"Unsupported method: {request_method}"
 
     return response_data
 
@@ -79,11 +96,23 @@ def create_response(response_data: dict) -> bytes:
         f"HTTP/1.1 {code} {RESPONSE_CODES[code]}",
         f"Date: {date_fmt}",
         f"Server: Python",
-        f"Content-Length: 0",
-        f"Content-Type: text/html; charset=UTF-8",
-        "",
-        "",
+        # f"Last Modified: {date_fmt}",
+        # "Connection: Keep-Alive",
     ]
+
+    # Check for redirect target
+    if 'location' in response_data:
+        response_lines.append(f"Location: {response_data['location']}")
+
+    # Check for message body
+    if response_data['body'] is not None:
+        # Check for file contents
+        if 'length' in response_data:
+            response_lines.append(f"Content-Length: {response_data['length']}")
+            response_lines.append(f"Content-Type: {response_data['type']}")
+
+        response_lines.append(response_data['body'])
+
     response = '\r\n'.join(response_lines)
     print(response)
     return bytes(response, 'utf-8')
@@ -92,10 +121,9 @@ def create_response(response_data: dict) -> bytes:
 if __name__ == '__main__':
     args = get_args()
     if args.request:
-        print("Parsing sample request")
         client_request = sys.stdin.buffer.read()
-        response_data = process_request(client_request)
-        server_response = create_response(response_data)
+        data = process_request(client_request)
+        server_response = create_response(data)
         exit(0)
 
     ip, port = args.ip, args.port
